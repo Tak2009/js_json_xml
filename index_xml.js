60dc7ce407786bf94a40540465a47999382f54e3
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-07 14:02:18 +0100

    working on index_xml

--- index_xml.js
+++ index_xml.js
@@ -11,43 +11,64 @@ populateHeader(superHeroes);
 showHeroes(superHeroes);
 }
 
-function populateHeader(jsonObj) {
-var myH1 = document.createElement('h1');
-myH1.textContent = jsonObj['squadName'];
-header.appendChild(myH1);
-var myPara = document.createElement('p');
-myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-header.appendChild(myPara);
-}
+const header = document.querySelector('header');
+    const section = document.querySelector('section');
 
-function showHeroes(jsonObj) {
-var heroes = jsonObj['members'];
-for (var i = 0; i < heroes.length; i++) {
-var myArticle = document.createElement('article');
-var myH2 = document.createElement('h2');
-var myPara1 = document.createElement('p');
-var myPara2 = document.createElement('p');
-var myPara3 = document.createElement('p');
-var myList = document.createElement('ul');
-
-myH2.textContent = heroes[i]['name']; //heroes[i].name;
-myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-myPara2.textContent = 'Age: ' + heroes[i].age;
-myPara3.textContent = 'Superpowers:';
     
-var superPowers = heroes[i].powers;
-for (var j = 0; j < superPowers.length; j++) {
-  var listItem = document.createElement('li');
-  listItem.textContent = superPowers[j];
-  myList.appendChild(listItem);
-}
+    const request = new XMLHttpRequest();
+    request.open('GET', "json.xml");
+    request.send();
 
-myArticle.appendChild(myH2);
-myArticle.appendChild(myPara1);
-myArticle.appendChild(myPara2);
-myArticle.appendChild(myPara3);
-myArticle.appendChild(myList);
 
-section.appendChild(myArticle);
-  }
-}
+    request.onload = function() {
+    const superHeroes = request.response;
+    populateHeader(superHeroes);
+    // showHeroes(superHeroes);
+    }
+
+    function populateHeader(jsonObj) {
+    // var myH1 = document.createElement('h1');
+    // myH1.textContent = jsonObj['squadName'];
+    // header.appendChild(myH1);
+
+    const docelem = xmlhttp.responseXML.documentElement;
+    const list = docelem.getElementsByTagName("root");
+    const myPara = document.createElement('p');
+    const townNodes = list.getElementsByTagName("homeTown");
+
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+    }
+
+  //   function showHeroes(jsonObj) {
+  //   var heroes = jsonObj['members'];
+      
+  //   for (var i = 0; i < heroes.length; i++) {
+  //   var myArticle = document.createElement('article');
+  //   var myH2 = document.createElement('h2');
+  //   var myPara1 = document.createElement('p');
+  //   var myPara2 = document.createElement('p');
+  //   var myPara3 = document.createElement('p');
+  //   var myList = document.createElement('ul');
+
+  //   myH2.textContent = heroes[i]['name']; //heroes[i].name;
+  //   myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+  //   myPara2.textContent = 'Age: ' + heroes[i].age;
+  //   myPara3.textContent = 'Superpowers:';
+        
+  //   var superPowers = heroes[i].powers;
+  //   for (var j = 0; j < superPowers.length; j++) {
+  //     var listItem = document.createElement('li');
+  //     listItem.textContent = superPowers[j];
+  //     myList.appendChild(listItem);
+  //   }
+
+  //   myArticle.appendChild(myH2);
+  //   myArticle.appendChild(myPara1);
+  //   myArticle.appendChild(myPara2);
+  //   myArticle.appendChild(myPara3);
+  //   myArticle.appendChild(myList);
+
+  //   section.appendChild(myArticle);
+  //   }
+  // }
