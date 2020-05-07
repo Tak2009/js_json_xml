140fa5c7b8872cb1f05db4ea1dddf7d7aa6d61e3
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-07 18:02:28 +0100

    xml tag modified

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 free formatter
 https://www.freeformatter.com/json-to-xml-converter.html#ad-output
+
+for xml test, add Live Server extention
--- index_xml.js
+++ index_xml.js
@@ -1,20 +1,23 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
 const request = new XMLHttpRequest();
+
 request.open('GET', "json.xml");
 request.send();
 
 request.onload = () => {
 const heroesGeneralInfo = request.responseXML.documentElement;
-debugger
+const heroesList = heroesGeneralInfo.getElementsByTagName("members");
+
+// debugger
 populateHeader(heroesGeneralInfo);
-// showHeroes(superHeroes);
+showHeroes(heroesList);
 }
 
 const populateHeader = (heroesGeneralInfo) => {
 const myH1 = document.createElement('h1');
 const squadName = heroesGeneralInfo.getElementsByTagName("squadName");
-myH1.textContent = squadName[0].firstChild.nodeValue // myH1.textContent = squadName[0].textContent
+myH1.textContent = squadName[0].firstChild.nodeValue // = myH1.textContent = squadName[0].textContent
 header.appendChild(myH1);
 const myPara = document.createElement('p');
 const homeTownNodes = heroesGeneralInfo.getElementsByTagName("homeTown");
@@ -23,35 +26,33 @@ myPara.textContent = 'Hometown: ' + homeTownNodes[0].firstChild.nodeValue + ' //
 header.appendChild(myPara);
 }
 
-  //   function showHeroes(jsonObj) {
-  //   var heroes = jsonObj['members'];
+const showHeroes = (heroesList) => {
+  debugger
+  for (let i = 0; i < heroesList.length; i++) {
+    const myArticle = document.createElement('article');
+    const myH2 = document.createElement('h2');
+    const myPara1 = document.createElement('p');
+    const myPara2 = document.createElement('p');
+    const myPara3 = document.createElement('p');
+    const heroDetails = document.createElement('ul');
+
+    myH2.textContent = heroesList[i].getElementsByTagName("formed"); //heroes[i].name;
+    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara3.textContent = 'Superpowers:';
       
-  //   for (var i = 0; i < heroes.length; i++) {
-  //   var myArticle = document.createElement('article');
-  //   var myH2 = document.createElement('h2');
-  //   var myPara1 = document.createElement('p');
-  //   var myPara2 = document.createElement('p');
-  //   var myPara3 = document.createElement('p');
-  //   var myheroesGeneralInfo = document.createElement('ul');
+    const superPowers = heroes[i].powers;
+    for (const j = 0; j < superPowers.length; j++) {
+      const heroesGeneralInfoItem = document.createElement('li');
+      heroesGeneralInfoItem.textContent = superPowers[j];
+      myheroesGeneralInfo.appendChild(heroesGeneralInfoItem);
+    }
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myheroesGeneralInfo);
 
-  //   myH2.textContent = heroes[i]['name']; //heroes[i].name;
-  //   myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-  //   myPara2.textContent = 'Age: ' + heroes[i].age;
-  //   myPara3.textContent = 'Superpowers:';
-        
-  //   var superPowers = heroes[i].powers;
-  //   for (var j = 0; j < superPowers.length; j++) {
-  //     var heroesGeneralInfoItem = document.createElement('li');
-  //     heroesGeneralInfoItem.textContent = superPowers[j];
-  //     myheroesGeneralInfo.appendChild(heroesGeneralInfoItem);
-  //   }
-
-  //   myArticle.appendChild(myH2);
-  //   myArticle.appendChild(myPara1);
-  //   myArticle.appendChild(myPara2);
-  //   myArticle.appendChild(myPara3);
-  //   myArticle.appendChild(myheroesGeneralInfo);
-
-  //   section.appendChild(myArticle);
-  //   }
-  // }
+    section.appendChild(myArticle);
+  }
+}
